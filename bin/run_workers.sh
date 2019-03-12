21940e9d02a5b0aacd00e7285cf77e5b2c4dc22b
Evan Sarmiento <esarmien@g.harvard.edu>
2019-03-12 12:52:11 -0400

    Add some changes for testing * Separate CAS and balance config using Include/IncludeOptional * Modify run_workers to determind number of instances on dyno boot * Move run_workers.sh to /bin

--- bin/run_workers.sh
+++ bin/run_workers.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-
 set -e
+DYNO_CORES=$(grep -c processor /proc/cpuinfo 2>/dev/null)
+CORES=${SHINY_NUM_PROCS:-${DYNO_CORES}}
+PORT_START=12000
+# Index starts at 0
+PORT_END=$(($PORT_START + $(($CORES - 1))))
 
-for port in {12000..12004}
+function BalanceMembers() {
+    local PROTOCOL="${1}"
+    for port in $(seq $PORT_START $PORT_END)
+    do
+        # Use port as the route number for simplicity.
+        echo "BalancerMember ${PROTOCOL}://127.0.0.1:${port} route=r${port}"
+    done
+}
+
+cat <<EOF > /app/apache/etc/apache2/instances.conf
+<Proxy balancer://webapp>
+$(BalanceMembers "http")
+ProxySet stickysession=route
+</Proxy>
+
+<Proxy balancer://websocket>
+$(BalanceMembers "ws")
+ProxySet stickysession=route
+</Proxy>
+EOF
+
+for port in $(seq $PORT_START $PORT_END)
 do
 	echo "Starting worker on $port..."
 	R -f /app/run.R --gui-none --no-save --args $port &
