95273b4acea169c9c506d06d9283dd211e45310c
Evan Sarmiento <esarmien@g.harvard.edu>
2019-09-06 13:04:49 -0400

    Add rmarkdown precompilation

---
+++ bin/compileMarkdown.R
@@ -0,0 +1,5 @@
+rmarkdownFiles <- list.files(path=".", pattern = "*.Rmd", recursive = TRUE, full.names=TRUE)
+rmarkdownFilesLibIgnored <- rmarkdownFiles[ !grepl("packrat", rmarkdownFiles) ]
+for (rmd in rmarkdownFilesLibIgnored) {
+    rmarkdown::render(rmd)
+}
