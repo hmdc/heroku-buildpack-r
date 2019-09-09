21aa000be36d8fed72c6d0e3f07491880bef50b0
Evan Sarmiento <esarmien@g.harvard.edu>
2019-09-09 14:25:23 -0400

    Prune packages

--- bin/boot.sh
+++ bin/boot.sh
@@ -9,20 +9,6 @@ mkdir -p /app/apache/logs
 mkdir -p /app/apache/var/cache
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
-mv /app/.root/usr/bin/pandoc /app/.root/usr/bin/pandoc.new
-cat<<EOF > /app/.root/usr/bin/pandoc
-#!/bin/bash
-#ARGS_RELATIVE=\$(echo "\$@"|sed -e 's/\/app\///g')
-#echo "==Old Args==" >> /tmp/pandoc.log
-#echo "\$@" >> /tmp/pandoc.log
-#echo "==New Args==" >> /tmp/pandoc.log
-#echo "\$ARGS_RELATIVE" >> /tmp/pandoc.log
-#echo "==Files==" >> /tmp/pandoc.log
-ls -alR /tmp >> /tmp/pandoc.log
-pwd >> /tmp/pandoc.log
-/usr/bin/pandoc.new \$ARGS_RELATIVE --verbose 2>&1 | tee -a /tmp/pandoc.log
-EOF
-chmod a+x /app/.root/usr/bin/pandoc
 
 COMMAND="${@:$n}"
 echo "Launching ${COMMAND}..."
